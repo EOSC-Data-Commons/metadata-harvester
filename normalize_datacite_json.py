fc3f4f93c35fc4c939ff83ef0c2f463c93777fea
Tobias Schweizer <tobias.schweizer@switch.ch>
2025-08-06 16:38:44 +0200

    feat (transform): try to make transformation more generic for different fields

--- normalize_datacite_json.py
+++ normalize_datacite_json.py
@@ -1,29 +1,30 @@
 import json
 
-def make_object(entry: list | object):
-    #print(entry)
+
+def make_object(entry: list | object, field_name: str):
+    # print(entry)
     if isinstance(entry, list):
-        res =  list(map(lambda fi: {'title': fi}, entry))
+        res = list(map(lambda fi: {field_name: fi}, entry))
         return res
     else:
         return entry
 
-def make_array(field: dict | list):
-    #print(json.dumps(field))
+
+def make_array(field: dict | list, field_name: str):
+    # print(json.dumps(field))
 
     if isinstance(field, dict):
-        print('dict')
-        res =  list(map(make_object, field.values()))
+        # print('dict')
+        res = list(map(lambda val: make_object(val, field_name), field.values()))
         return [x for sublist in res for x in sublist]
     elif isinstance(field, list):
-        print('list')
+        # print('list')
         return field
 
+
 def normalize_datacite_json(input: dict):
-    #print(json.dumps(input))
+    # print(json.dumps(input))
 
-    return make_array(input['titles'])
-
-
-
-
+    return {
+        'titles': make_array(input['titles'], 'title'),
+        'subjects': make_array(input['subjects'], 'subject')}
